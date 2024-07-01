720cc5a409e4a5dabd887fb124c5111dd9f4cc58
bestwishforyou95 <bestwishforyou95@gmail.com>
2024-07-01 14:07:09 +0700

    update

--- api/main.js
+++ api/main.js
@@ -27,11 +27,11 @@ server.use(
 // Configure multer for parsing multipart/form-data
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    const uploadPath = path.join(__dirname, "uploads");
+    const uploadPath = path.join(__dirname, "../uploads");
     if (!fs.existsSync(uploadPath)) {
       fs.mkdirSync(uploadPath, { recursive: true });
     }
-    cb(null, path.join(__dirname, "../uploads"));
+    cb(null, uploadPath);
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`);
