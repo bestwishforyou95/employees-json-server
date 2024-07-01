8c71234a62892719a79d12361b4efd93915b3371
bestwishforyou95 <bestwishforyou95@gmail.com>
2024-07-01 14:21:22 +0700

    update

--- api/main.js
+++ api/main.js
@@ -20,7 +20,7 @@ server.get("/echo", (req, res) => {
 server.use(
   jsonServer.rewriter({
     "/api/*": "/$1",
-    "/employees/:employeeId": "/:employeeId",
+    "/blog/:resource/:id/show": "/:resource/:id",
   })
 );
 
