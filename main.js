4b4cd778426dcee8f552dac05c6157c0697131cd
bestwishforyou95 <bestwishforyou95@gmail.com>
2024-07-01 11:01:05 +0700

    update

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const qs = require("qs");
 const fs = require("fs");
 const path = require("path");
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "../db.json"));
+const router = jsonServer.router(path.join(__dirname, "./db.json"));
 const middlewares = jsonServer.defaults();
 
 // Set default middlewares (logger, static, cors and no-cache)
@@ -26,7 +26,7 @@ server.use(
 // Configure multer for parsing multipart/form-data
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    const uploadPath = path.join(__dirname, "../tmp/cdn");
+    const uploadPath = path.join("/tmp", "cdn");
     if (!fs.existsSync(uploadPath)) {
       fs.mkdirSync(uploadPath, { recursive: true });
     }
@@ -63,14 +63,14 @@ server.post("/employees/uploads", upload.array("files"), (req, res) => {
   }
   const filePaths = req.files.map((file, index) => ({
     id: index + 1,
-    cdnUrl: `/tmp/cdn/${file.filename}`,
+    cdnUrl: `/cdn/${file.filename}`,
     displayOrder: index,
   }));
   res.status(200).json({ files: filePaths });
 });
 
 // Serve static files from the 'cdn' directory
-server.use("/tmp/cdn", express.static(path.join(__dirname, "../tmp/cdn")));
+server.use("/cdn", express.static(path.join("/tmp", "cdn")));
 
 // Use multer before jsonServer.bodyParser to handle multipart/form-data
 server.use(upload.none());
@@ -118,7 +118,7 @@ router.render = (req, res) => {
 };
 
 // Use default router
-server.use(router);
+server.use("/api", router);
 const PORT = process.env.PORT || 9090;
 server.listen(PORT, () => {
   console.log("JSON Server is running");
