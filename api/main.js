9629f5d4d469c81385d17edade9decc98b1895bf
bestwishforyou95 <bestwishforyou95@gmail.com>
2024-07-01 15:50:46 +0700

    update

--- api/main.js
+++ api/main.js
@@ -5,9 +5,7 @@ const qs = require("qs");
 const fs = require("fs");
 const path = require("path");
 const server = jsonServer.create();
-const router = jsonServer.router(
-  fs.readFileSync(path.join(__dirname, "../db.json"))
-);
+const router = jsonServer.router(path.join(__dirname, "../db.json"));
 const middlewares = jsonServer.defaults();
 
 // Set default middlewares (logger, static, cors and no-cache)
